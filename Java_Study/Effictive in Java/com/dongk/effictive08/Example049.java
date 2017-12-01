f5f76275c0130b552af0f740a48276bedd525949
123dongkang0 <1152853133@qq.com>
2017-12-01 11:25:16 +0800

    2017-12-01

---
+++ Java_Study/Effictive in Java/com/dongk/effictive08/Example049.java
@@ -0,0 +1,17 @@
+package com.dongk.effictive08;
+
+import java.util.Comparator;
+
+/**
+ * 第49条：基本类型优先于装箱基本类型
+ * 
+ *  1.基本类型、装箱基本类型的区别。
+ *    1.1 基本类型只有值；而装箱基本类型则具有和它们值不同的同一性。
+ *    1.2 基本类型通常比装箱基本类型更加节省时间和空间。
+ */
+public class Example049 {
+    public static void main(String args[]){
+    	
+    	
+    }
+}
