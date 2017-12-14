4280cfe93c8f5a45adcee7f29e85b2de53d74757
123dongkang0 <1152853133@qq.com>
2017-12-14 21:26:51 +0800

    Effictive

--- Java_Study/JAVA-基础知识/com/effictive05/Example025.java
+++ Java_Study/JAVA-基础知识/com/effictive05/Example025.java
@@ -1,5 +1,6 @@
 package com.effictive05;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,7 +12,9 @@ import org.junit.Test;
  *  1)、数组和泛型相比，有两个重要的不同点
  *    1.1）、数组是协变的，即如果Sub[]就是Super的子类型，那么数组类型Sub[]就是Super的子类型。而泛型
  *          是不可变的。
- *    1.2）、数组是具体化的        
+ *    1.2）、数组是具体化的 ,因此数组会在运行时才知道并且检查它们元素的约束类型；
+ *    
+ *  2)、数组和泛型混合使用的时候，如果出现了编译错误或者警告，第一反应应该是使用列表来代替数组。
  */
 public class Example025 {
     
@@ -39,4 +42,60 @@ public class Example025 {
 		String s = stringLists[0].get(0);  //在这里会有类型转换的异常，为了防止这种情况发生，所以第一行给了一个编译时错误
 		*/
 	}
+	
+	/**
+	 * 3）、假设有一个同步列表和一个函数(拥有两个与该列表同类型的参数值)，
+	 *      然后编写一个方法reduce，并使用函数apply处理这个列表(假设apply为求和运算，那么reduce就是求列表中所有元素的值)
+	 * 
+	 */
+	
+	/**
+	 * 3.1）、没有泛型时的代码： （使用同步代码块）
+	 */
+	static Object reduce01(List list, Function f, Object initVal){
+		synchronized(list){
+			Object result = initVal;
+			for(Object o : list)
+				result = f.apply(result, o);
+			return result;
+		}
+	}
+	
+	/**
+	 * 3.2）、 在备份上执行apply方法(不适用同步代码块)
+	 *     利用List的toArray方法(它在内部锁定列表)
+	 *     
+	 *     下面使用数组来实现备份，在运行过程中，可能会出现ClassCaseException
+	 */
+	static <E> Object reduce02(List<E> list, Function<E> f, E initVal){
+		E[] sanpshot = (E[])list.toArray(); //进行强制转换后，会有警告出现
+		E result = initVal;
+		for(E e:sanpshot)
+			result = f.apply(result, initVal);
+		return result;
+	}
+	
+	/**
+	 * 3.3）、 在备份上执行apply方法
+	 *     利用List
+	 *     这段代码虽然有点 冗长，但是可以确定它在运行时不会得到ClassCastException异常，
+	 *     所以是值得的。
+	 */
+	static <E> Object reduce03(List<E> list, Function<E> f, E initVal){
+		List<E> sanpshot;
+		synchronized(list){
+			sanpshot = new ArrayList<E>(list);
+		}
+		E result = initVal;
+		for(E e:sanpshot)
+			result = f.apply(result, e);
+		return result;
+	}
+	
+	
 }
+
+interface Function<T>{
+	T apply(T arg1, T arg2);
+}
+
