54cc8773ac4414d13fe128836d4318ff971a2f56
123dongkang0 <1152853133@qq.com>
2018-03-06 11:06:46 +0800

    lambda

--- Java_Study/JAVA-基础知识/com/java8/lambda/lambda_introduce001.java
+++ Java_Study/JAVA-基础知识/com/java8/lambda/lambda_introduce001.java
@@ -1,9 +1,21 @@
 package com.java8.lambda;
 
+import org.junit.Test;
+
 /**
  * 1.理解lambda表达式的JAVA实现，有两个结构非常关键，第一个是 lambda表达式自身，
  *   第二个是函数式接口。
  */
 public class lambda_introduce001 {
+	
+	@Test
+	public void test01() {
+		MyNumber myNum = () -> 123.45;
+		System.out.println(myNum.getValue());
+	}
 
+}
+
+interface MyNumber{
+	double getValue();
 }
