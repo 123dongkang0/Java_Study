018b788143e28ce0958002c33bc22e1ce80715ba
123dongkang0 <1152853133@qq.com>
2018-03-06 23:23:13 +0800

    lambda

--- Java_Study/JAVA-基础知识/com/java8/lambda/lambda_introduce001.java
+++ Java_Study/JAVA-基础知识/com/java8/lambda/lambda_introduce001.java
@@ -10,12 +10,39 @@ public class lambda_introduce001 {
 	
 	@Test
 	public void test01() {
-		MyNumber myNum = () -> 123.45;
+		MyNumber myNum;
+		
+	    myNum = () -> 123.45;
 		System.out.println(myNum.getValue());
+		
+	    myNum = () -> Math.random() * 100;
+		System.out.println(myNum.getValue());
+		
+		//error 函数式接口的返回值是double
+		//myNum = () -> "Hello lambda";
+	}
+	
+	/**
+	 *带参数的lambda表达式 
+	 */
+	@Test
+	public void test02() {
+		NumercTest isEven = (n) -> (n%2) == 0;
+		if(isEven.test(10)) System.out.println("10 is even");
+		if(!isEven.test(9)) System.out.println("9 is not even");
+		
+		NumercTest isNonNeg = (n) -> n>=0;
+		if(isNonNeg.test(1)) System.out.println(" 1 is non-negative");
+		if(!isNonNeg.test(-1)) System.out.println(" -1 is negative");
 	}
 
 }
 
 interface MyNumber{
 	double getValue();
+}
+
+
+interface NumercTest{
+	boolean test(int n);
 }
