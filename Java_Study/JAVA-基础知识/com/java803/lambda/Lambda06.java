8847070bc883a8dbe423025632a8b3e86b04f085
123dongkang0 <1152853133@qq.com>
2018-04-23 19:16:55 +0800

    MethodRef

--- Java_Study/JAVA-基础知识/com/java803/lambda/Lambda06.java
+++ Java_Study/JAVA-基础知识/com/java803/lambda/Lambda06.java
@@ -3,11 +3,14 @@ package com.java803.lambda;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 import org.junit.Test;
+
+import com.java8.model.Apple;
 
 /**
 * <b>Description:
@@ -33,9 +36,47 @@ public class Lambda06 {
 	public void test01(){
 		
 		List<String> str = Arrays.asList("a","E","A","D","E");
-		str.sort((s1,s2) -> s1.compareToIgnoreCase(s2));
 		
+		//str.sort((s1,s2) -> s1.compareToIgnoreCase(s2));
+		
+		//等效方法引用如下
+		str.sort(String :: compareToIgnoreCase);
 		str.forEach(System.out :: println);
 	}
 	
+	/**
+	 * 3.6.2 构造函数的引用
+	 *     Function 只是适合有一个参数的构造函数。
+	 */
+	@Test
+	public void test02() {
+		//等价于,Function<Integer, Apple> c1 = (weight) -> new Apple(weight); 
+		Function<Integer, Apple> c2 = Apple::new;
+		Apple a2 = c2.apply(110);
+	}
+	
+	@Test
+	public void test03() {
+		List<Integer> weights = Arrays.asList(7,3,4,10);
+		List<Apple> apples = map(weights, Apple::new);
+		apples.forEach(System.out::println);
+	}
+	
+	public static List<Apple> map(List<Integer> list, Function<Integer, Apple> f){
+		List<Apple> result = new ArrayList<>();
+		for(Integer e : list) {
+			result.add(f.apply(e));
+		}
+		return result;
+	}
+	
+	/**
+	 * BiFunction 适合具有两个参数的构造函数
+	 */
+	@Test
+	public void test04() {
+		BiFunction<Integer,String, Apple> c3 = Apple::new;
+		Apple a3 = c3.apply( 110,"green");
+		
+	}
 }
---
+++ Java_Study/JAVA-基础知识/com/java803/lambda/Lambda07.java
@@ -0,0 +1,24 @@
+package com.java803.lambda;
+
+/**
+* <b>Description:
+*       3.7 Lambda和方法引用实战。
+*       
+*          1.我们尝试解决下面的这个问题.
+*              使用不同的排序策略给一个Apple列表排序。
+*              
+*              思路：     Java8的API已经为你提供了一个List可用的sort方法（你不用自己去实现它，那么最困难的部分已经搞定了）。
+*                   我们关心的是如何把排序策略传递给sort方法呢？
+* 
+* </b><br> 
+* @author:dongk
+* @version 1.0
+* @Note
+* <b>ProjectName:</b> Java_Study
+* <br><b>PackageName:</b> com.java803.lambda
+* <br><b>ClassName:</b> Lambda07
+* <br><b>Date:</b> 2018年4月23日 下午7:06:02
+*/
+public class Lambda07 {
+
+}
